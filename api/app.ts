381200a1dc57d2cf4fa9a6de8a20c4965416747d
Icefoxes <gary.guo.china@gmail.com>
2022-12-15 12:16:41 +0800

    add jest for api

--- api/app.ts
+++ api/app.ts
@@ -1,16 +1,22 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 import { flowRoute, teamRoute } from './routes';
 import cors from 'cors';
 import BodyParser from 'body-parser';
+import path from 'path';
 
 export const createApp = () => {
     const app: Express = express();
     app.use(cors());
-
+    console.log(path.join(__dirname, 'public'));
+    app.use(express.static(path.join(__dirname, 'public')))
     app.use(BodyParser.urlencoded({ extended: false }));
     app.use(BodyParser.json());
 
     app.use('/api/v1/flows', flowRoute);
     app.use('/api/v1/teams', teamRoute);
+    app.use('/', (req: Request, res: Response) => {
+        res.sendFile('index.html')
+    });
+   
     return app;
 }
---
+++ api/test/flow.test.ts
@@ -0,0 +1,6 @@
+const sum = (a: number, b: number) => a + b;
+
+
+it("test if flow can be added", () => {
+    expect(sum(1, 2)).toBe(3);
+})
